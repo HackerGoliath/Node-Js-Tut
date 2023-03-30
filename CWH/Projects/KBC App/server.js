0acdf342f274667a40474a15bb5c7d1861b564cf
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-03-30 17:30:09 +0530

    Project: Quiz App

---
+++ CWH/Projects/KBC App/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.listen(8000, () => {
+    console.log('Server running on port  http://localhost:8000');
+});
